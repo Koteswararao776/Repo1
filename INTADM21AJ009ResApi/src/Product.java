37f540f2599a2daba57e6b5b662dcde5b4e3cf7e
Koteswararao776 <durgasaipanchakarla776@gmail.com>
2021-05-07 14:46:02 +0530

    this is my second commit for creating product class

---
+++ INTADM21AJ009ResApi/src/Product.java
@@ -0,0 +1,20 @@
+
+public class Product {
+	
+	private int productId;
+	private String productName;
+	public int getProductid() {
+		return productId;
+	}
+	public void setProductid(int productid) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+
+}
